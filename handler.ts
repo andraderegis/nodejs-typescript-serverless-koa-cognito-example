d785e42aa13bf4fac37ba3eb4465977b569e3bd9
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2022-03-13 14:50:19 -0300

    chore: project setup

---
+++ handler.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import serverlessExpress from '@vendia/serverless-express';
+import { APIGatewayEvent, Context } from 'aws-lambda';
+
+import app from './infra/server';
+
+let serverlessExpressInstance: any;
+
+export const server = async (event: APIGatewayEvent, context: Context) => {
+  if (!serverlessExpressInstance) {
+    serverlessExpressInstance = serverlessExpress({ app: app as any });
+  }
+
+  return serverlessExpressInstance(event, context);
+};
